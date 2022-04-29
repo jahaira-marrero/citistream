606939660682d34f5b1c6afeddfc996dbf5e56d3
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-28 22:18:32 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -3,3 +3,5 @@ import pandas as pd
 import requests
 streamlit.header("Hello data")
 
+data = requests.get("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
+streamlit.write(data)
