ded72723c7589f8b6867e79dcf604e2c42144698
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-28 22:28:38 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -3,6 +3,6 @@ import pandas as pd
 import requests
 streamlit.header("State Testing Historical Data Set")
 
-data = requests.get("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-streamlit.text(data.json())
+data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
+streamlit.dataframe(data)
 
