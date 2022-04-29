6938676702ab23ded155eca214af64ac8aa69687
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-29 10:35:55 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -6,7 +6,7 @@ import requests
 
 streamlit.header("State Testing Historical Data Set")
 data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-data = data.set_index("State")
+#data = data.set_index("State")
 
 date_state_df = data.groupby(by=["date"]).sum()
 
