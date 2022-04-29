dd825d774aa235c864954575db237f3fec3ad070
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-29 10:49:37 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -6,12 +6,12 @@ import requests
 
 streamlit.header("State Testing Historical Data Set")
 data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-#data = data.set_index("State")
+data = data.set_index("State")
 
 date_state_df = data.groupby(by=["date", "State"]).sum()
 
-date_selected = streamlit.multiselect("Choose a date:", list(date_state_df))
-date_to_show = data.loc[date_selected]
+date_selected = streamlit.multiselect("Choose a date:", list(data.index))
+date_to_show = date_state_df.loc[date_selected]
 
 streamlit.dataframe(date_state_df)
 
