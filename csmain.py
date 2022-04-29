dfe9551bbd469db3cbd04766381d686527ca95b4
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-29 10:01:33 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -7,11 +7,10 @@ import requests
 streamlit.header("State Testing Historical Data Set")
 data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
 
-def get_column_names(data):
-  for col in data.columns:
-    return col
+for col in data.columns:
+  streamlit.write(col)
  
-streamlit.write(get_column_names(data))
+
 streamlit.multiselect("Choose your filters:", list(data.index))
 
 streamlit.dataframe(data)
