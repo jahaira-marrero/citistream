5812d8590911c01639f9db5ceaf7ee15b8eb8194
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-29 10:25:42 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -6,11 +6,14 @@ import requests
 
 streamlit.header("State Testing Historical Data Set")
 data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-
+data = data.set_index("date")
 for col in data.columns:
   streamlit.write(col)
   
-streamlit.write(data.groupby(by=["date", "State"]).sum())
+date_state_df = data.groupby(by=["date", "State"]).sum()
+
+  
+streamlit.write
  
 
 streamlit.multiselect("Choose your filters:", list(data.index))
