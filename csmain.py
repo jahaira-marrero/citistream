f560685c03b943fa0c903ce18ff2a64877690dbf
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-28 22:26:34 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -4,6 +4,5 @@ import requests
 streamlit.header("State Testing Historical Data Set")
 
 data = requests.get("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-data_normalzed = pd.json_normalize(data.json())
+streamlit.text(data.json())
 
-streamlit.dataframe(data_normalized)
