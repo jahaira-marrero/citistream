ccc1027fc2e424f9ce07aad01e01e4772bb9bebd
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-28 22:50:40 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -1,12 +1,16 @@
 import streamlit
 import pandas as pd
 import requests
+from pyspark.sql.functions import *
+from pyspark.sql.types import InterType, DateType
+
 streamlit.header("State Testing Historical Data Set")
 
-data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-#data = data.set_index('State')
-data = data.set_index('date')
+#data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
+data_df = spark.read.format('csv').option('inferSchema', True).option('header', True).load("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
+#data = data.set_index('date')
 date_data = data\.groupby(pd.TimeGrouper('M'))\.agg({'combined':''.join})
 #streamlit.multiselect("Choose your filters:", list(data.index))
-streamlit.dataframe(data)
+#streamlit.dataframe(data)
+data_df.show()
 
