acfc778529dc974c074ce51072ba2b0a2fd2da82
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-29 09:34:37 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -6,9 +6,7 @@ import requests
 
 streamlit.header("State Testing Historical Data Set")
 data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-#data_df = spark.read.format('csv').option('inferSchema', True).option('header', True).load("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4bGzRySZ2aaXlJgie?entityName=States%20Testing%20Historical&exportFormat=csv")
-data = data.set_index('date')
-#date_data = data\.groupby(pd.TimeGrouper('M'))\.agg({'combined':''.join})
+streamlit.write(data.head())
 streamlit.multiselect("Choose your filters:", list(data.index))
 #streamlit.dataframe(data)
 streamlit.dataframe(data)
