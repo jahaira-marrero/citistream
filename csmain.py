5fcdd83241e8e3c6b89bc0e9bb5b26607fedb64f
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-29 11:04:27 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -9,10 +9,8 @@ data = pd.read_csv("https://knowi.com/api/data/ipE4xJhLBkn8H8jisFisAdHKvepFR5I4b
 data = data.set_index("date")
 
 date_state_df = data.groupby(by=["date", "State"]).sum()
-
 date_selected = streamlit.multiselect("Choose a date:", list(data.index))
-date_to_show = date_state_df.drop(columns=["grade"])
-date_to_show = date_state_df[date_selected]
+date_to_show = date_state_df.loc[date_selected]
 
 streamlit.dataframe(date_to_show.style.highlight_max(axis=0))
 
