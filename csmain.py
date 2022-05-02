247dd8205b5558c4c5c5c36f00e52fb8f1130f73
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-02 16:58:15 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -8,16 +8,19 @@ import pydeck as pdx
 import requests
 
 DATA_URL= ("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
+DATA_RESPONSE = pd.json_normalize(DATA_URL.json())
 
-def load_data(nrows):
-    data = pd.json_normalize(DATA_URL.json(), nrows  = nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-    return data
+st.text(DATA_RESPONSE)
 
-data = load_data(100000)
+# def load_data(nrows):
+#     data = pd.json_normalize(DATA_URL.json(), nrows  = nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+#     return data
+
+# data = load_data(100000)
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC.")
-st.text(data)
+# st.text(data)
 
            
 
