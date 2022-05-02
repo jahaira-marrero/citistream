a86de7aed22c8bf27b7e0757cbd1e53f977e9861
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-02 16:39:42 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -6,7 +6,11 @@ import pydeck as pdx
 import plotly.express as px
 import request
 
-DATA_URL = ("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
+DATA_URL = requests.get("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
+
+def load_data(nrows):
+    data = pd.json_normalized(DATA_URL.json(),nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    return data
 
 st. title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC."
