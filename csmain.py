5ecd67f09d85893f5f0255786f30baa85abea696
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-02 18:13:28 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -12,7 +12,7 @@ d = pd.json_normalize(data.json())
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC.")
-st.write(data)
+st.write(d)
 
 
 
