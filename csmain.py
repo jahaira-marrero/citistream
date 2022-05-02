79f28b638b61fc6a4a0e6d0ba9772ad586daeef5
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-02 17:26:55 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -9,13 +9,13 @@ import requests
 
 DATA_URL= requests.get("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
 DATA_RESPONSE = pd.json_normalize(DATA_URL.json())
-st.dataframe(DATA_RESPONSE)
+# st.dataframe(DATA_RESPONSE)
 
-# def load_data(nrows):
-#     data = pd.json_normalize(DATA_URL.json(), nrows  = nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-#     return data
+def load_data(nrows):
+    data = pd.DataFrame(DATA_RESPONSE, nrows =nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    return data
 
-# data = load_data(100000)
+data = load_data(100000)
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC.")
