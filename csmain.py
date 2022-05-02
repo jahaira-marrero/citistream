7d22a872c096a1475d91af798b7216dad42714b1
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-02 16:52:35 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -3,15 +3,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdx
-import plotly.express as px
+#import plotly.express as px
 # from sodapy import Socrata
 import requests
 
-
-# client = Socrata("data.cityofnewyork.us", None)
-# results = client.get("h9gi-nx95", limit=2000)
-# results_df = pd.DataFrame.from_records(results)
-# DATA_URL = requests.get("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
 
 def load_data(nrows):
     data = pd.json_normalized(DATA_URL.json(), nrows  = nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
