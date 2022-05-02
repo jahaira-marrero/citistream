4e9b6615476d4e9d8cf159fb8cce1a4d6895a3f3
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-02 18:01:03 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -4,12 +4,10 @@ import pandas as pd
 import numpy as np
 import pydeck as pdx
 #import plotly.express as px
-# from sodapy import Socrata
 import requests
 
-#DATA_URL= requests.get("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
 url_path = "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
-data = pd.read_json(url_path, convert_dates=['crash_date', 'crash_time'])
+data = pd.read_json(url_path, convert_dates=['crash_date', 'crash_time'], date_unit='m', lines=True, nrows=100000)
 #d = pd.to_datetime(data['CRASH_DATE', 'CRASH_TIME'])
 
 st.title("Motor Vehicle Collisions in New York City")
