83970b14153dc5067484dacec162b08d95bfa2d3
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-02 16:48:35 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -4,21 +4,24 @@ import pandas as pd
 import numpy as np
 import pydeck as pdx
 import plotly.express as px
-from sodapy import Socrata
+# from sodapy import Socrata
+import requests
 
 
-client = Socrata("data.cityofnewyork.us", None)
-results = client.get("h9gi-nx95", limit=2000)
-results_df = pd.DataFrame.from_records(results)
+# client = Socrata("data.cityofnewyork.us", None)
+# results = client.get("h9gi-nx95", limit=2000)
+# results_df = pd.DataFrame.from_records(results)
 # DATA_URL = requests.get("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
 
-# def load_data(nrows):
-#     data = pd.json_normalized(DATA_URL.json(),nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-#     return data
+def load_data(nrows):
+    data = pd.json_normalized(DATA_URL.json(), nrows  = nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    return data
+
+data = load_data(100000)
 
 st. title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC."
-st.dataframe(results_df)
+st.write(data)
 
            
 
