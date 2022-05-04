5e1cdec6af24ff22bce39e5b0c94418345d16909
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-04 16:20:58 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -11,12 +11,15 @@ url= "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC.")
 
-
-#st.cache(persist=True)
+st.cache(persist=True)
 data = pd.read_json(url, convert_dates=['crash_date', 'crash_time'])
 data.dropna(subset = ['latitude', 'longitude'], inplace=True)
 
 st.write(data)
+
+st.header("Where are the most people injured in NYC?")
+injured_people = st.slider("Number if persons injured in vehicle collisions", 0,19)
+st.map(data.query("injured_persons >- @injured_people")[["latitide", "longitude"]].dropna(how="any"))
 
 
 
