0dc0ca6ea075df7ddfc5cb127d17e527918ddc26
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-04 16:05:26 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -7,16 +7,19 @@ import pydeck as pdx
 import requests
 
 url= "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
-data = pd.read_json(url, convert_dates=['crash_date', 'crash_time'])
-data.dropna(subset = ['latitude', 'longitude'], inplace=True)
-lowercase = lambda x: str(x).lower()
-data.rename(lowercase, axis = 'columns', inplace=True)
-date.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
-
-
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC.")
+
+
+st.cache(persist=True)
+def load_data(nrows):
+           data = pd.read_json(url, convert_dates=['crash_date', 'crash_time'], lines=True, chunksize=nrows)
+           data.dropna(subset = ['latitude', 'longitude'], inplace=True)
+           return data
+
+data = load_data(100000)
+
 st.write(data)
 
 
