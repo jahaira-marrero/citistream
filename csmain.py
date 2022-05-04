12876ac2fb6a5ecbf99e4494792741779957aedb
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-04 15:43:26 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -8,13 +8,13 @@ import requests
 
 url= "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
 data = pd.read_json(url)
-st.dataframe(data)
-#data['crash_date'] = pd.to_datetime(data['crash_date'])
+#st.dataframe(data)
+data['crash_date'] = pd.to_datetime(data['crash_date'], infer_datetime_format=True)
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC.")
 #st.write(data)
-#st.write(data.head())
+st.write(data.head())
 
 
 
