26bd1d93535393920011c7fbdb3cd192dcbf0ab1
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-04 15:48:35 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -7,14 +7,14 @@ import pydeck as pdx
 import requests
 
 url= "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
-data = pd.read_json(url)
-#st.dataframe(data)
-data['crash_date'] = pd.to_datetime(data['crash_date'], infer_datetime_format=True)
+data = pd.read_json(url, convert_dates=['crash_date'])
+st.dataframe(data)
+#data['crash_date'] = pd.to_datetime(data['crash_date'], infer_datetime_format=True)
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be used to analyze motorvehicle collisions in NYC.")
 #st.write(data)
-st.write(data.head())
+#st.write(data.head())
 
 
 
