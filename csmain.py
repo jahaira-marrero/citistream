82603bf14eefffcd85f7db37d708c8c9eecae1b5
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-05 12:02:24 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -17,7 +17,7 @@ data.crash_date = data.crash_date.str.split('T').str[0]
 #data.crash_time = data.crash_time.dt.time
 
 data.crash_time = data.crash_time.astype(str)
-#data.crash_time = data.crash_time.str.split('T').str[1]
+data.crash_time = data.crash_time.str.split(' ').str[1]
 
 
 
