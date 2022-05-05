88db6417982cb74504fabaa777d45387d0a25159
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-05 17:34:39 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -15,7 +15,7 @@ st.cache(persist=True)
 
 data = pd.read_json(url)
 data.crash_date= data.crash_date.str.split('T').str[0]
-data.crash_time = data.crash_time.dt.time
+data.crash_time = data.crash_time.dt.hour
 data.dropna(subset = ['latitude', 'longitude'])
 st.dataframe(data)
 
