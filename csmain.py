3b795291da76278f1692a919a4b7ddbf99ab4617
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-05 12:23:24 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -1,4 +1,4 @@
-
+import datetime
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -15,7 +15,7 @@ st.cache(persist=True)
 data = pd.read_json(url)
 data.crash_date = data.crash_date.str.split('T').str[0]
 
-data.crash_time = data.crash_time.to_timestamp
+data.crash_time = data.crash_time.apply(lambda x: x.strftime('%H:%M'))
 #data.crash_time = data.crash_time.dt.time
 #data.crash_time = data.crash_time.astype(str)
 #data.crash_time = data.crash_time.str.split(' ').str[1]
