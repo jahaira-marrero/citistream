61d48b7ce150e14c980e48900a01099ed7791b29
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-05 12:10:31 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -14,11 +14,12 @@ st.markdown("This application is a streamlit dashboard that can be used to analy
 st.cache(persist=True)
 data = pd.read_json(url)
 data.crash_date = data.crash_date.str.split('T').str[0]
-#data.crash_time = data.crash_time.dt.time
 
-data.crash_time = data.crash_time.astype(str)
+#data.crash_time = data.crash_time.dt.time
+#data.crash_time = data.crash_time.astype(str)
 #data.crash_time = data.crash_time.str.split(' ').str[1]
 #data.crash_time = data.crash_time.dt.hour
+
 st.write(data.dtypes())
 
 data.dropna(subset = ['latitude', 'longitude'], inplace=True)
