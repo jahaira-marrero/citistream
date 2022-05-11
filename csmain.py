134547056fae99d7b620e46d49d0da1165a424da
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-11 16:35:59 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import snowflake.connector
 #import plotly.express as px
 import requests
 
@@ -10,12 +11,12 @@ st.title("Citibike Trips")
 
 
 def get_sblist():
-           with my_cnx.cursor() as my_cur:
-             my_cur.execute("select * from trips limit 100")
-             return my_cur.fetchall()
+   with my_cnx.cursor() as my_cur:
+           my_cur.execute("select * from trips")
+           return my_cur.fetchall()
                       
 
-if streamlit.button("Get Trips"):
+if streamlit.button('Get Trips'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_sblist()
     my_cnx.close()
