029aa6cd30871b67afefd5a8c0078806c9b8d1d0
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-05-11 16:56:38 -0400

    Update csmain.py

--- csmain.py
+++ csmain.py
@@ -9,19 +9,16 @@ import requests
 
 streamlit.title("Citibike Trips")
 
-
-
-# def get_sblist():
-#    with my_cnx.cursor() as my_cur:
-#            my_cur.execute("select * from trips")
-#            return my_cur.fetchall()
-                      
-
-# if streamlit.button('Get Trips'):
-#     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#     my_data_rows = get_sblist()
-#     my_cnx.close()
-#     streamlit.dataframe(my_data_rows)
+def get_sblist():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from trips")
+      return my_cur.fetchall()
+        
+if streamlit.button('Get Trips'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_sblist()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
            
 
 
